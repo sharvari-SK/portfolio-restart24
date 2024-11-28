a1b7b93fc5d8ee090619df0c340ee645eca06992
sharvari-SK <sharvarikurudi@gmail.com>
2024-11-28 10:43:58 +0530

    basic template

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div >
+    <nav id='nav-bar'>
+      <div  className='my-logo'> Sharvari Kurudi </div>
+      <div>
+      <ui className = "nav-links">
+      <li><a href='#about'> About</a></li>
+      <li><a href='#experience'> Experience</a></li>
+      <li><a href='#projects'> Projects</a></li>
+      <li><a href='#contact'> Contact</a></li>
+      </ui>
+      </div>
+      </nav>
+
+    // </div>
   );
 }
 
